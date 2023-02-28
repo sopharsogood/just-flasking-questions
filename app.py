5959882f6683c088bbd26464a1391dbdf443e76b
sophie <justanothersophie@gmail.com>
2023-02-28 17:42:14 -0500

    ORM fixes, added __repr__ defs to models

--- app.py
+++ app.py
@@ -9,26 +9,34 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, unique = True)
+    username = db.Column(db.String, unique = True)
     questions = db.relationship('Question', backref = 'user')
     answers = db.relationship('Answer', backref = 'user')
     date_created = db.Column(db.DateTime)
 
+    def __repr__(self) -> str:
+        return '<User %>' % self.username
+
 class Question(db.Model):
     __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key = True)
-    author_id = db.Column(db.Integer, db.ForeignKey(users.id))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title = db.Column(db.String)
     content = db.Column(db.String)
     date_created = db.Column(db.DateTime)
 
+    def __repr__(self):
+        return '<User %>' % self.title
+
 class Answer(db.Model):
     __tablename__ = 'answers'
     id = db.Column(db.Integer, primary_key = True)
-    author_id = db.Column(db.Integer, db.ForeignKey(users.id))
-    title = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     content = db.Column(db.String)
     date_created = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<User %>' % self.content
 
 
 @app.route('/')

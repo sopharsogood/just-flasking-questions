6b4ba7bccf40df1287d9fe7445a540ee098e3739
sophie <justanothersophie@gmail.com>
2023-02-28 17:19:26 -0500

    User class model

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///development.db'
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, unique = True)
+    date_created = db.Column(db.DateTime)
+
+
 
 @app.route('/')
 def index():

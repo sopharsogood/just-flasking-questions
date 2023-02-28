6473301012d4cbd7334332f77f4332fb44b340a1
sophie <justanothersophie@gmail.com>
2023-02-28 16:33:39 -0500

    fixed from Flask to from flask; added app.run command

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from Flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "This is a placeholder for the site's index"
+
+if __name__ == "__main__":
+    app.run(debug=true)

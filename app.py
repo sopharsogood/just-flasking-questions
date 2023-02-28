0d1eb7798e63b377a9b4238f15d8c209b3bca16c
sophie <justanothersophie@gmail.com>
2023-02-28 17:33:41 -0500

    bugfix: should not be a 3 in sqlite

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///development.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
